04eff201db02c99ac650657fabb663cc57990c87
JackPifer <jpifer01@gmail.com>
2023-12-12 03:01:41 -0600

    Re-add client folder

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import Login from "./login.js"
+import Dashboard from "./Dashboard.js"
+const code = new URLSearchParams(window.location.search).get("code");
+function App() {
+  return code ? <Dashboard code={code} /> : <Login />;
+}
+
+export default App
